447b3493673e44eb64749c997811db37ad340f4d
Adriana Camarotto <adriana.camarotto@gmail.com>
2024-01-29 16:50:29 +0000

    created artists API artists.json

---
+++ script.js
@@ -0,0 +1,25 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then((response) => response.json())
+        // .then((result) => displayResults(result))
+}
+
+// function displayResults() { 
+//     resultPlaylist.classList.add('hidden');
+//     result
+// }
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if(searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+     }
+ requestApi(searchTerm);
+});
